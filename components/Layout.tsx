cf6f382d0f46d5005f3a2d70dcfccc020647fcd8
RObert Sus <you@example.com>
2022-12-19 10:05:21 +0100

    basic styling Layout, add sample data and images

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,27 +10,28 @@ function Layout({title, children}:any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+    <div className='flex flex-col min-h-screen justify-between'>   
       {/* HEADER */}
       <header>
-        <nav>
-            <Link href="\">
+        <nav className='flex items-center justify-between h-16 px-4 bg-slate-300 shadow-md'>
+            <Link className="font-bold text-xl" href="\">
                 amazon
             </Link>
             <div>
-                <Link href="/cart">Cart</Link>
+                <Link className="p-2" href="/cart">Cart</Link>
                 <Link href="/login">Login</Link>
             </div>
         </nav>
       </header>
 
       {/* DYNAMIC COMPONENT */}
-      <main>{children}</main>
+      <main className='container m-auto mt-4 px-4'>{children}</main>
 
       {/* FOOTER */}
-      <footer>
+      <footer className='flex justify-center items-center h-16 bg-slate-300 shadow-md shadow-gray shadow-inner'>
         Copyright &copy; 2022 Amazon Shop
       </footer>
-      
+    </div>
 
     </div>
   )
