8eb5cac9975ad6fee1cf1d3d76d09450839fcd50
Robert Sus <sus_robert@poczta.onet.pl>
2023-01-14 12:46:00 +0100

    add context cart items, add handler add product

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,17 @@
-import Head from "next/head";
-import Link from "next/link";
-import React from "react";
+import Head from 'next/head';
+import Link from 'next/link';
+import React, { useContext } from 'react';
 
-import { ShoppingBagIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { ShoppingBagIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+import { Store } from '../utils/Store';
 
 function Layout({ title, children }: any) {
+  const { state, dispatch } = useContext(Store);
+
   return (
     <div>
       <Head>
-        <title>{title ? title + " - E-Shop" : "E-Shop"}</title>
+        <title>{title ? title + ' - E-Shop' : 'E-Shop'}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -22,9 +25,14 @@ function Layout({ title, children }: any) {
             <div className="relative">
               <Link className="p-4" href="/cart">
                 <ShoppingBagIcon className="inline h-8 w-8 hover:text-gray-500" />
-                <span className="absolute font-bold -ml-3 text-center leading-5 text-xs w-5 h-5 rounded-full bg-red-400">
-                  {0}
-                </span>
+                {state.cart.cartItems.length > 0 ? (
+                  <span className="absolute font-bold -ml-3 text-center leading-5 text-xs w-5 h-5 rounded-full bg-red-400">
+                    {state.cart.cartItems.reduce(
+                      (acc, item) => acc + item.quantity,
+                      0
+                    )}
+                  </span>
+                ) : null}
               </Link>
 
               <Link href="/login">
--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import Layout from "../../components/Layout";
-import data from "utils/data";
-import Link from "next/link";
-import Image from "next/image";
+import React, { useContext } from 'react';
+import Layout from '../../components/Layout';
+import data from '../../utils/data';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Store } from '../../utils/Store';
 
 function ProductScreen({ product }: any) {
+  const { state, dispatch } = useContext(Store);
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out ofd stock.');
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -37,9 +51,11 @@ function ProductScreen({ product }: any) {
           </div>
           <div className="mb-2 flex justify-between">
             <div>Status</div>
-            <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
+            <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
           </div>
-          <button className="primary-button w-full">Add to cart</button>
+          <button className="primary-button w-full" onClick={addToCartHandler}>
+            Add to cart
+          </button>
         </div>
       </div>
     </Layout>
